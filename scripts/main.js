32b0a3083682f743ce9aefe6a72c41d4d83c8af2
clivann <ivan.clavijo2@gmail.com>
2017-05-01 13:59:29 -0400

    added more scripts

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,10 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Bienvenidos'
-
-document.querySelector('h1').onclick = console.log('hola')
+var myImage = document.getElementById('subbutton');
+var midfi = "images/middlefinger2.jpeg";
+myImage.onclick = function() {
+  var mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/subscribe.png') {
+    myImage.setAttribute ('src', midfi );
+  } else {
+    myImage.setAttribute ('src','images/subscribe.png');
+  }
+}
